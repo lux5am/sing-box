e89771b639eec5dcfeef719656beea98800c6173
PuerNya <safarier@outlook.com>
2024-05-26 01:09:00 +0800

    use origin fqdn when destination is fqdn
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- route/route.go
+++ route/route.go
@@ -533,7 +533,7 @@ func (r *Router) actionSniff(
 		)
 		if err == nil {
 			//goland:noinspection GoDeprecation
-			if action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
+			if !metadata.Destination.IsFqdn() && action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
 				metadata.Destination = M.Socksaddr{
 					Fqdn: metadata.SniffHost,
 					Port: metadata.Destination.Port,
@@ -630,7 +630,7 @@ func (r *Router) actionSniff(
 				}
 				if metadata.Protocol != "" {
 					//goland:noinspection GoDeprecation
-					if action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
+					if !metadata.Destination.IsFqdn() && action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
 						metadata.Destination = M.Socksaddr{
 							Fqdn: metadata.SniffHost,
 							Port: metadata.Destination.Port,

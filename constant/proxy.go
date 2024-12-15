c0b29c86ba6b23721f7280ca6380f1e9cf3bd88e
lux5am <lux.5am@yandex.com>
2024-12-15 12:16:16 +0800

    add inner detour connection to the connection trackers
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- constant/proxy.go
+++ constant/proxy.go
@@ -1,6 +1,7 @@
 package constant
 
 const (
+	TypeInner              = "inner"
 	TypeTun                = "tun"
 	TypeRedirect           = "redirect"
 	TypeTProxy             = "tproxy"
@@ -56,6 +57,8 @@ const (
 
 func ProxyDisplayName(proxyType string) string {
 	switch proxyType {
+	case TypeInner:
+		return "Inner"
 	case TypeTun:
 		return "TUN"
 	case TypeRedirect:

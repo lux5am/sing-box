1a873814fcf4f240363d526ab8dc7cad86bf0633
lux5am <lux.5am@yandex.com>
2024-12-18 11:31:48 +0800

    add outbound type `pass`
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- route/route.go
+++ route/route.go
@@ -646,6 +646,14 @@ match:
 		var routeOptions *R.RuleActionRouteOptions
 		switch action := currentRule.Action().(type) {
 		case *R.RuleActionRoute:
+			if selectedOutbound, loaded := r.outbound.Outbound(action.Outbound); loaded {
+				if selectedOutbound.Type() == C.TypeSelector {
+					selectedOutbound = selectedOutbound.(adapter.SelectorGroup).Selected(N.NetworkTCP)
+				}
+				if selectedOutbound != nil && selectedOutbound.Type() == C.TypePass {
+					continue
+				}
+			}
 			routeOptions = &action.RuleActionRouteOptions
 		case *R.RuleActionRouteOptions:
 			routeOptions = action

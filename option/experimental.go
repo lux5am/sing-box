095b20cc5c572aecee6e2dc2e1de36ee8c302d7b
lux5am <lux.5am@yandex.com>
2025-01-21 21:07:14 +0800

    add `urltest_unified_delay` in experimental config
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- option/experimental.go
+++ option/experimental.go
@@ -15,6 +15,8 @@ type ExperimentalOptions struct {
 	Debug     *DebugOptions     `json:"debug,omitempty"`
 	Timeout   *TimeoutOptions   `json:"timeout,omitempty"`
 	Constant  *ConstantOptions  `json:"constant,omitempty"`
+
+	URLTestUnifiedDelay bool `json:"urltest_unified_delay,omitempty"`
 }
 
 func (o *ExperimentalOptions) Apply() {
@@ -24,6 +26,7 @@ func (o *ExperimentalOptions) Apply() {
 	if o.Constant != nil {
 		o.Constant.Apply()
 	}
+	C.URLTestUnifiedDelay = o.URLTestUnifiedDelay
 }
 
 type CacheFileOptions struct {

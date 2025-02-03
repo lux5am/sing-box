2b8573550ce5ef53b85e68aae34f7e5eceb896ed
lux5am <lux.5am@yandex.com>
2025-02-03 21:22:04 +0800

    set authoritative flag in dns response
    some client discard response without aa
    
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- dns/client.go
+++ dns/client.go
@@ -456,6 +456,7 @@ func (c *Client) finishExchange(transport adapter.DNSTransport, operation *excha
 			response.SetEdns0(responseEDNSOpt.UDPSize(), responseEDNSOpt.Do())
 		}
 	}
+	response.Authoritative = true
 	logExchangedResponse(c.logger, ctx, response, timeToLive)
 	return response, nil
 }

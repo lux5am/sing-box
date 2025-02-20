2c1ac4d27dc5b00094f06b939ea3690a31c061e8
lux5am <lux.5am@yandex.com>
2025-02-21 07:41:27 +0800

    add `null-ip` reject method for dns rule action
    can be used for adblock ruleset
    
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- constant/rule.go
+++ constant/rule.go
@@ -45,4 +45,5 @@ const (
 	RuleActionRejectMethodDefault = "default"
 	RuleActionRejectMethodDrop    = "drop"
 	RuleActionRejectMethodReply   = "reply"
+	RuleActionRejectMethodNullIP  = "null-ip"
 )

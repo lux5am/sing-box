d7dd878fd5a6d9dd164d5934e62374b526bcb486
lux5am <lux.5am@yandex.com>
2025-06-09 07:16:11 +0800

    add outbound type `loadbalance`
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- constant/proxy.go
+++ constant/proxy.go
@@ -50,6 +50,7 @@ const (
 	TypeSelector = "selector"
 	TypeURLTest  = "urltest"
 	TypeFallback = "fallback"
+	TypeLoadBalance = "loadbalance"
 )
 
 func ProxyDisplayName(proxyType string) string {
@@ -122,6 +123,8 @@ func ProxyDisplayName(proxyType string) string {
 		return "URLTest"
 	case TypeFallback:
 		return "Fallback"
+	case TypeLoadBalance:
+		return "LoadBalance"
 	default:
 		return "Unknown"
 	}

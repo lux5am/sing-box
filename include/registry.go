0f84469cb61fb92a4287fee358c5e83ee69d5e90
lux5am <lux.5am@yandex.com>
2025-06-09 07:16:11 +0800

    add outbound type `loadbalance`
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- include/registry.go
+++ include/registry.go
@@ -88,6 +88,7 @@ func OutboundRegistry() *outbound.Registry {
 	group.RegisterSelector(registry)
 	group.RegisterURLTest(registry)
 	group.RegisterFallback(registry)
+	group.RegisterLoadBalance(registry)
 
 	socks.RegisterOutbound(registry)
 	http.RegisterOutbound(registry)

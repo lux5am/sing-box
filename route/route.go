303f214c8117b58b0811942a5cdbc1fe087d03b5
lux5am <lux.5am@yandex.com>
2026-08-09 11:02:39 +0800

    add `skip_domain` to route action sniff
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- route/route.go
+++ route/route.go
@@ -793,6 +793,10 @@ func (r *Router) actionSniff(
 		metadata.SnifferNames = action.SnifferNames
 		metadata.SniffError = err
 		if err == nil {
+			if action.MatchSkip(metadata) {
+				r.logger.DebugContext(ctx, "skip sniffed domain: ", metadata.SniffHost)
+				metadata.SniffHost = ""
+			}
 			//goland:noinspection GoDeprecation
 			if !metadata.Destination.IsFqdn() && action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
 				metadata.Destination = M.Socksaddr{
@@ -917,6 +921,10 @@ func (r *Router) actionSniff(
 		}
 	finally:
 		if err == nil {
+			if action.MatchSkip(metadata) {
+				r.logger.DebugContext(ctx, "skip sniffed domain: ", metadata.SniffHost)
+				metadata.SniffHost = ""
+			}
 			//goland:noinspection GoDeprecation
 			if !metadata.Destination.IsFqdn() && action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
 				metadata.Destination = M.Socksaddr{
